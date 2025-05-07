05a1e1993700cf0d41e4f42a1c2eff2f57e81f35
Rahul Pandita <rpandita1999@outlook.com>
2025-05-07 11:06:06 +0530

    use @Bean to indicate that a method produces a bean to be managed by the spring container

--- simpleGame/src/main/java/com/rahul/simpleGame/config/HelloWorldConfiguration.java
+++ simpleGame/src/main/java/com/rahul/simpleGame/config/HelloWorldConfiguration.java
@@ -1,7 +1,13 @@
 package com.rahul.simpleGame.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HelloWorldConfiguration {
+
+    @Bean
+    public String name() {
+        return "Chintu";
+    }
 }
