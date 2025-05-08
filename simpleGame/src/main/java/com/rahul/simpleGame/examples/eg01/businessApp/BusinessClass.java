f321e529b5d1c2b9b136b7344663d4d594f20b76
Rahul Pandita <rpandita1999@outlook.com>
2025-05-08 08:19:54 +0530

    use @Autowired to perform setter based dependency injection

--- simpleGame/src/main/java/com/rahul/simpleGame/examples/eg01/businessApp/BusinessClass.java
+++ simpleGame/src/main/java/com/rahul/simpleGame/examples/eg01/businessApp/BusinessClass.java
@@ -6,14 +6,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class BusinessClass {
 
-    @Autowired
+//    @Autowired  // FIELD INJECTION
     private Dependency1 dependency1;
 
-    @Autowired
+//    @Autowired
     private Dependency2 dependency2;
+
+    @Autowired
+    public void setDependency1(Dependency1 dependency1) {
+        this.dependency1 = dependency1;
+    }
+
+    @Autowired
+    public void setDependency2(Dependency2 dependency2) {
+        this.dependency2 = dependency2;
+    }
 
     @Override
     public String toString() {
         return "Using " + dependency1 + " and " + dependency2 ;
     }
+
+
 }
