b642a02bd53d49d6bd85f0e742cf8660fb90bc0a
Rahul Pandita <rpandita1999@outlook.com>
2025-05-08 12:50:28 +0530

    map a url to the method

--- spring-boot-primer/src/main/java/com/rahul/spring_boot_primer/CourseController.java
+++ spring-boot-primer/src/main/java/com/rahul/spring_boot_primer/CourseController.java
@@ -4,6 +4,7 @@ package com.rahul.spring_boot_primer;
 // create a simple rest api
 // get back some data -> Course: id, name, author
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -12,6 +13,9 @@ import java.util.List;
 @RestController
 public class CourseController {
 
+    // need to map a url to this specific method
+    // requests to that url will result in call to this method
+    @RequestMapping("/courses")
     public List<Course> retrieveAllCourses() {
         return Arrays.asList(new Course(1, "Learn the electric guitar", "Jimmy Hendrix"),
                              new Course(2, "Learn the piano", "Mozart"));
