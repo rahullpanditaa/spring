e89cfeed5ce86abb274278f12c12b5a60dab4a24
Rahul Pandita <rpandita1999@outlook.com>
2025-05-08 15:11:21 +0530

    previous commit stupid; now, create custom url for in-memory h2 db and create a schema for a table

---
+++ jpa-hibernate/src/test/java/com/rahul/jpa_hibernate/JpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.jpa_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
