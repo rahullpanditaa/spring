a0b54d3c1fdaa6f95f63b4c46eb58ca84e9f0a05
Rahul Pandita <rpandita1999@outlook.com>
2025-05-09 15:21:49 +0530

    another controller that returns an endpoint mapped to a login page

--- webApplication/src/main/java/com/rahul/webApplication/helloWorld/HelloWorldController.java
+++ webApplication/src/main/java/com/rahul/webApplication/helloWorld/HelloWorldController.java
@@ -5,36 +5,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller
 public class HelloWorldController {
 
     // request to this api endpoint will result
     // in this method being called
+    // @ResponseBody tells spring to write the return value directly to http response body
+    // instead of resolving it as a view name
     @RequestMapping("hello")
     @ResponseBody
     public String hello() {
         return "Hello World";
     }
 
-//    @RequestMapping("hello-html")
-//    @ResponseBody // this annotation tells spring to write the return values directly to the http response body
-    // instead of resolving it as the name of a view to be rendered
-    public String helloHtml() {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("<html>");
-        stringBuilder.append("<head>");
-        stringBuilder.append("<title>Practice Page</title>");
-        stringBuilder.append("</head>");
-        stringBuilder.append("<body>");
-        stringBuilder.append("<h1>Welcome to My Page</h1>");
-        stringBuilder.append("<p>This is a paragraph of text for testing purposes.</p>");
-        stringBuilder.append("<p>Writing return value (in this case, some html) directly to http response body.</p>");
-        stringBuilder.append("</body>");
-        stringBuilder.append("</html>");
-        return stringBuilder.toString();
-    }
-
-    // using JSP to return a view
+    // returning an html VIEW
     @RequestMapping("hello-html")
     public String helloHtmlJsp() {
         return "hello";  // this will be resolved as a view name (hello.jsp)
