03ccb48ff4dbb5857d3806ef309465b01a38e458
Rahul Pandita <rpandita1999@outlook.com>
2025-05-10 11:29:25 +0530

    separate methods for the same endpoint for a GET and POST request

--- webApplication/src/main/java/com/rahul/webApplication/login/LoginController.java
+++ webApplication/src/main/java/com/rahul/webApplication/login/LoginController.java
@@ -4,13 +4,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("/login")
+    // only use this method for a GET request (displaying the login page)
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String loginPage() {
         return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String goToWelcomePage() {
+        return "welcome";
     }
 }
