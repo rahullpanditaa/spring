14727f6c3216fdb1221e012e0871541cc1280628
Rahul Pandita <rpandita1999@outlook.com>
2025-05-10 11:39:27 +0530

    using ModelMap to display login credentials in the html view

--- webApplication/src/main/java/com/rahul/webApplication/login/LoginController.java
+++ webApplication/src/main/java/com/rahul/webApplication/login/LoginController.java
@@ -3,8 +3,10 @@ package com.rahul.webApplication.login;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -17,7 +19,9 @@ public class LoginController {
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String goToWelcomePage() {
+    public String goToWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
+        model.put("name", name);
+        model.put("password", password);
         return "welcome";
     }
 }
