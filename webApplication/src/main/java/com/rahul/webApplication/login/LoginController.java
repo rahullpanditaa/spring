6d043e403b2d5f5940943034f1668d3fca6bb63a
Rahul Pandita <rpandita1999@outlook.com>
2025-05-10 07:51:21 +0530

    query parameters - using @RequestParam to indicate that a method parameter should be bound to a web request/query parameter

--- webApplication/src/main/java/com/rahul/webApplication/login/LoginController.java
+++ webApplication/src/main/java/com/rahul/webApplication/login/LoginController.java
@@ -2,12 +2,17 @@ package com.rahul.webApplication.login;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
+    // http://localhost:8080/login?name=rahul
+    // name passed to login controller
+    // need to tell login controller that this specific method gets a request parameter
     @RequestMapping("/login")
-    public String loginPage() {
+    public String loginPage(@RequestParam String name) {  // @RequestParam - indicates that a method parameter should be bound to a web request param
+        System.out.println("Request param is: " + name);
         return "login";
     }
 }
