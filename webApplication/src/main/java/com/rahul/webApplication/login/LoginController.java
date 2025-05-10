90c46adc67a86e3781abf270c98ccec25620c402
Rahul Pandita <rpandita1999@outlook.com>
2025-05-10 11:17:19 +0530

    creating a basic login form view

--- webApplication/src/main/java/com/rahul/webApplication/login/LoginController.java
+++ webApplication/src/main/java/com/rahul/webApplication/login/LoginController.java
@@ -1,20 +1,16 @@
 package com.rahul.webApplication.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class LoginController {
 
-    // http://localhost:8080/login?name=rahul
-    // name passed to login controller
-    // need to tell login controller that this specific method gets a request parameter
     @RequestMapping("/login")
-    public String loginPage(@RequestParam String name, ModelMap model) {  // @RequestParam - indicates that a method parameter should be bound to a web request param
-        model.put("name", name);
-        System.out.println("Request param is: " + name);
+    public String loginPage() {
         return "login";
     }
 }
