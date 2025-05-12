d0d8115f8bf4c64a20c3a7d63a644efab66d44bf
Rahul Pandita <rpandita1999@outlook.com>
2025-05-12 15:02:38 +0530

    modify login controller - one endpoint, the root mapped to a method that returns the welcome html view

--- webApplication/src/main/java/com/rahul/webApplication/login/LoginController.java
+++ webApplication/src/main/java/com/rahul/webApplication/login/LoginController.java
@@ -1,43 +1,16 @@
 package com.rahul.webApplication.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class LoginController {
 
-    private AuthenticationService authenticationService;
-
-    public LoginController(AuthenticationService authenticationService) {
-        this.authenticationService = authenticationService;
-    }
-
-    // only use this method for a GET request (displaying the login page)
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String loginPage() {
-        return "login";
-    }
-
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String goToWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
-        model.put("name", name);
-        model.put("password", password);
-
-        // authentication logic
-        // name - razumikhin
-        // password - dunechka
-        var authService = authenticationService.authenticate(name,password); // boolean value
-//        return authService ? "welcome" : "login";
-        if (authService) {
-            return "welcome";
-        }
-        model.put("errorMessage", "Invalid Credentials. Please try again");
-        return "login";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String loginPage(ModelMap model) {
+        model.put("name", "Razumikhin");
+        return "welcome";
     }
 }
