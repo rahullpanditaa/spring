3fd7f5e86a5b459325ce0124c601ab8a6678072a
Rahul Pandita <rpandita1999@outlook.com>
2025-05-14 11:52:24 +0530

    another rest api endpoint with a path parameter

--- restfulWebServices/src/main/java/com/rahul/restfulWebServices/helloWorld/HelloWorldController.java
+++ restfulWebServices/src/main/java/com/rahul/restfulWebServices/helloWorld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.rahul.restfulWebServices.helloWorld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping("/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean("Hey there " + name);
     }
 }
