8d46a7d10ac07e996cc3a68ff60b413a449877f1
Rahul Pandita <rpandita1999@outlook.com>
2025-05-14 11:23:24 +0530

    a simple rest api

---
+++ restfulWebServices/src/main/java/com/rahul/restfulWebServices/helloWorld/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.rahul.restfulWebServices.helloWorld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
