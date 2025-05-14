b65bb41084ff6f3bc454b83011bdb79cb6464852
Rahul Pandita <rpandita1999@outlook.com>
2025-05-14 13:19:37 +0530

    creating rest api endpoints to GET and POST to h2 database

--- restfulWebServices/src/main/java/com/rahul/restfulWebServices/socialMedia/UserController.java
+++ restfulWebServices/src/main/java/com/rahul/restfulWebServices/socialMedia/UserController.java
@@ -3,6 +3,8 @@ package com.rahul.restfulWebServices.socialMedia;
 import com.rahul.restfulWebServices.socialMedia.entities.User;
 import com.rahul.restfulWebServices.socialMedia.repositories.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,5 +21,15 @@ public class UserController {
     @GetMapping("/users")
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    @PostMapping("/users")
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/users/{id}")
+    public User findOne(@PathVariable Integer id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found with given id: " + id));
     }
 }
