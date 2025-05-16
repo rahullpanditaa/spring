a1d51c757c194f628d2e736ec13592869895f11a
Rahul Pandita <rpandita1999@outlook.com>
2025-05-16 08:15:17 +0530

    modified filter chain to show basic authentication popup, not a webpage when trying to access endpoint

---
+++ restfulWebServices/src/main/java/com/rahul/restfulWebServices/socialMedia/config/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.rahul.restfulWebServices.socialMedia.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        // all incoming requests should be authenticated
+        httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+
+        httpSecurity.httpBasic(withDefaults());
+
+        return httpSecurity.build();
+    }
+}
