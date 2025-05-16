23c0587cb66454611f91bd8b9fcb7791e42af0c9
Rahul Pandita <rpandita1999@outlook.com>
2025-05-16 16:16:58 +0530

    adding state to counter; useState hook

--- frontEnd/todo-app/src/components/counter/Counter.jsx
+++ frontEnd/todo-app/src/components/counter/Counter.jsx
@@ -1,13 +1,19 @@
+import { useState } from 'react'
 import './Counter.css'
 
 export default function Counter() {
 
+    const [count, setCount] = useState(0) // returns the current state value and a function to change the state val
+
     function incrementCounter() {
-        console.log("increment button clicked")
+        setCount(count + 1)
+        // console.log(count, setCount);
+        
+        // console.log("increment button clicked")
     }
     return (
         <div className="Counter">
-            <span className="count">0</span>
+            <span className="count">{count}</span>
             <div>
                 <button className="counterButton" onClick={incrementCounter}>+1 </button>
             </div>
