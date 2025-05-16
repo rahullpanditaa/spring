673946efeb5aa981c0beffb1a6f05ab5dfe23273
Rahul Pandita <rpandita1999@outlook.com>
2025-05-16 16:24:37 +0530

    adding another button that decrements counter

--- frontEnd/todo-app/src/components/counter/Counter.jsx
+++ frontEnd/todo-app/src/components/counter/Counter.jsx
@@ -7,15 +7,18 @@ export default function Counter() {
 
     function incrementCounter() {
         setCount(count + 1)
-        // console.log(count, setCount);
-        
-        // console.log("increment button clicked")
     }
+
+    function decrementCounter() {
+        setCount(count - 1)
+    }
+
     return (
         <div className="Counter">
             <span className="count">{count}</span>
             <div>
-                <button className="counterButton" onClick={incrementCounter}>+1 </button>
+                <button className="incrementCounterButton commonButtonStyle" onClick={incrementCounter}>+1</button>
+                <button className='decrementCounterButton commonButtonStyle' onClick={decrementCounter}>-1</button>
             </div>
         </div>
     )
