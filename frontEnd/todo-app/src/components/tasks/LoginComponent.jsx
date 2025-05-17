627fedf1f55d29e1e4104e3ffb336ec03acd3709
Rahul Pandita <rpandita1999@outlook.com>
2025-05-17 15:15:13 +0530

    added hardcoded authentication; conditional rendering of the appropriate message based on validity of credentials

--- frontEnd/todo-app/src/components/tasks/LoginComponent.jsx
+++ frontEnd/todo-app/src/components/tasks/LoginComponent.jsx
@@ -3,11 +3,19 @@ import { useState } from "react"
 export default function LoginComponent() {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [loginStatus, setLoginStatus] = useState(null)   // failure or success
 
     function IsLoginClicked() {
-        console.log("Login clicked")
-        console.log("username: ", username)
-        console.log("password: ", password)
+        const hardcodedUsername = "rahul"
+        const hardcodedPassword = "guy_montag"
+
+        if (username === hardcodedUsername && password === hardcodedPassword) {
+            console.log("login successful")
+            setLoginStatus("SUCCESS")
+        } else {
+            console.log("login failed. invalid credentials")
+            setLoginStatus("FAILURE")
+        }
     }
 
     return (
@@ -16,19 +24,24 @@ export default function LoginComponent() {
             <div>
                 <label>Username: </label>
                 <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)}></input>
-                <p>username being entered: {username}</p>
             </div>
             <div>
                 <label>Password: </label>
                 <input type="password" name="password" 
                        value={password} 
                        onChange={(e) => setPassword(e.target.value)}></input>
-                <p>password being entered: {password}</p>
             </div>
             <div>
                 <button type="button" name="login" onClick={IsLoginClicked}
                  disabled={!username || !password}>Login</button>
             </div>
+            {/* CONDITIONAL RENDERING */}
+            {loginStatus === "SUCCESS" && (
+                <div className="successMessage">Login success!</div>
+            )}
+            {loginStatus === "FAILURE" && (
+                <div className="errorMessage">Login Failed! Invalid credentials</div>
+            )}
         </div>
     )
     
