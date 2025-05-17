fd4014be46bc8687fad22b5d13bea07c2e6baf88
Rahul Pandita <rpandita1999@outlook.com>
2025-05-17 13:04:44 +0530

    tracking username and password by react state; inputs are controlled inputs

--- frontEnd/todo-app/src/components/tasks/LoginComponent.jsx
+++ frontEnd/todo-app/src/components/tasks/LoginComponent.jsx
@@ -1,17 +1,33 @@
+import { useState } from "react"
+
 export default function LoginComponent() {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    function IsLoginClicked() {
+        console.log("Login clicked")
+        console.log("username: ", username)
+        console.log("password: ", password)
+    }
+
     return (
         <div className="LoginComponent">
             <h3>please enter username and password</h3>
             <div>
                 <label>Username: </label>
-                <input type="text" name="username"></input>
+                <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)}></input>
+                <p>username being entered: {username}</p>
             </div>
             <div>
                 <label>Password: </label>
-                <input type="password" name="password"></input>
+                <input type="password" name="password" 
+                       value={password} 
+                       onChange={(e) => setPassword(e.target.value)}></input>
+                <p>password being entered: {password}</p>
             </div>
             <div>
-                <button type="button" name="login">Login</button>
+                <button type="button" name="login" onClick={IsLoginClicked}
+                 disabled={!username || !password}>Login</button>
             </div>
         </div>
     )
