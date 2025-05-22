6f08c87eaf71317c2ea65e83bf90caa87e5f13ac
Rahul Pandita <rpandita1999@outlook.com>
2025-05-22 08:08:48 +0530

    practicing sharing global state stored in context with multiple child components

--- frontEnd/todo-app/src/components/tasks/WelcomeComponent.jsx
+++ frontEnd/todo-app/src/components/tasks/WelcomeComponent.jsx
@@ -1,11 +1,16 @@
+import { useContext } from "react"
 import { useLocation } from "react-router-dom"
+import { AuthContext } from "./security/AuthContext"
 
 export default function WelcomeComponent() {
     const location = useLocation()
     const {username} = location.state || {username : "Guest"}  // accessing location via use location hook
+
+    const authContext = useContext(AuthContext)
     return (
         <div className="WelcomeComponent">
             <h1>Welcome, {username}</h1>
+            <p>current global state being stored in context is ${authContext.number}</p>
         </div>
     )
 }
