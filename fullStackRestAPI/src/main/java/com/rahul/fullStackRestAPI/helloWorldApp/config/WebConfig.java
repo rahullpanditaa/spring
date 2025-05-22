6a08dff3533921c594c38f28647fb8ae89e6aa5e
Rahul Pandita <rpandita1999@outlook.com>
2025-05-22 15:26:07 +0530

    react frontend connected to spring boot backend

--- fullStackRestAPI/src/main/java/com/rahul/fullStackRestAPI/helloWorldApp/config/WebConfig.java
+++ fullStackRestAPI/src/main/java/com/rahul/fullStackRestAPI/helloWorldApp/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080/hello-world")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET","POST","DELETE","PUT")
                 .allowedHeaders("*");
     }
