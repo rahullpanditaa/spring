8db7ea3036d7949c39427325ae05dd2e4e20da25
Rahul Pandita <rpandita1999@outlook.com>
2025-05-24 07:48:39 +0530

    added 2 more rest api endpoints; mapped to delete and get requests

--- fullStackRestAPI/src/main/java/com/rahul/fullStackRestAPI/todoApp/controllers/TodoController.java
+++ fullStackRestAPI/src/main/java/com/rahul/fullStackRestAPI/todoApp/controllers/TodoController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TodoController {
@@ -21,13 +22,27 @@ public class TodoController {
         return todoRepository.findByUsername(username);
     }
 
-//    @GetMapping("/users/{username}/todos/{id}")
-//    public ResponseEntity<Todo> retrieveTodoByIdOfGivenUser(@PathVariable String username, @PathVariable Integer id) {
-//        return todoRepository.findByUsernameAndId(username, id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
-//    }
-//
+//     returns an optional todos, since possible to not find a match in database
+    @GetMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Todo> retrieveTodo(@PathVariable String username, @PathVariable Integer id) {
+        Optional<Todo> todo = todoRepository.findById(id);
+
+        if (todo.isPresent()) {
+            return ResponseEntity.ok(todo.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
     @PostMapping("/users/{username}/todos")
     public void createTodoForGivenUser(@PathVariable String username, @RequestBody Todo todo) {
         todoRepository.save(todo);
+    }
+
+    @DeleteMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Void> deleteATodo(@PathVariable String username, @PathVariable Integer id) {
+        todoRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
